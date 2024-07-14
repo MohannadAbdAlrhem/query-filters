ae1c0bd51eec67dab54fe1d55b776dfe40128e9c
MouhannadAbdAlrhem <mouhannadabdalrhem@gmail.com>
2024-07-14 14:52:39 +0300

    Initial commit

---
+++ src/QueryFiltersServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mouhannad\QueryFilters;
+
+use Illuminate\Support\ServiceProvider;
+
+class QueryFiltersServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        dd('testPackage');
+    }
+}
